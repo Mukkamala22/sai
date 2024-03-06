b78aa743234b4d9d4b7c1f3afeddb2ef4f0f54a7
Mukkamala22 <162416154+Mukkamala22@users.noreply.github.com>
2024-03-06 09:48:45 +0000

    added car

--- pyhton.py
+++ pyhton.py
@@ -1,2 +1,53 @@
 print("hello")
 print("thankyou")
+class Car:
+    
+    def __init__(self,brand,model,year,fuel_type,CC,price,features,wheel_drive):
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.fuel_type = fuel_type
+        self.CC = CC
+        self.price = price
+        self.features = features
+        self.is4wd = wheel_drive
+    
+        
+    def car_info(self):
+        print("Car_info")
+        print("Brand :",self.brand)
+        print("Model :",self.model)
+        print("Manufacture Year :",self.year)
+        print("Fuel type :",self.fuel_type)
+        print("Engine capacity :", "{}".format(self.CC)+ ("cc" if self.fuel_type != "electric" else "KWH"))
+        print("Price :",self.price)
+        print("Features :",self.features)
+        print("wheel drive : 4WD") if self.is4wd == True else print("wheel drive : 2WD")
+        
+list_cars = [ ]   
+
+car = Car("Toyota","Innova",2001,"petrol",2.4,3000000,["2 Airbags","Sunroof"],False)
+car.car_info()
+list_cars.append(car)
+
+car = Car("TATA","safari",2004,"diesel",2.5,1800000,["6 Airbags","LCD screen"],True)
+car.car_info()
+list_cars.append(car)
+
+car = Car("Ford","Endeavour",2023,"petrol",3.0,2500000,["6 Airbags","Sunroof"],True)
+car.car_info()
+list_cars.append(car)
+
+car = Car("KIA","Seltos",2022,"petrol",1.5,1800000,["2 Airbags","Sunroof"],False)
+car.car_info()
+list_cars.append(car)
+
+car = Car("BMW","M5",2024,"petrol",4.0,10000000,["8 Airbags","ADAS"],True)
+car.car_info()
+list_cars.append(car)
+
+car = Car("Hyundai","Kona",2022,"electric",64,2300000,["6 Airbags","359L Bootspace"],False)
+car.car_info()
+list_cars.append(car)
+
+print(list_cars)
